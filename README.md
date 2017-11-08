bacb816f41ef5eabe079d6cf43c98c92f51d387b
Philippe Brossier <brossierp@gmail.com>
2017-11-08 13:55:32 +0000

    now dumoing msgs from queue into csv

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
+- before building:
+    - RabbitMQ details are defined in application.yml
+    - queue read is defined in action-instruction-inbound-flow.xml : see queueNames
+    - msgs read from queue are written to /Users/philippebrossier/code_perso/rabbitMQwork/queueDump.csv : see hard-coding in OutputServiceImpl
+
+
 - to build: mvn clean install
 
 
-- to run: java -jar target/tutorial-0.0.1-SNAPSHOT.jar
+- to run:
+    - /Users/philippebrossier/code_perso/rabbitMQwork/queueDump.csv requires deletion?
+    - java -jar target/tutorial-0.0.1-SNAPSHOT.jar
 
 
 - to test:
--- src/main/java/com/example/tutorial/service/impl/OutputServiceImpl.java
+++ src/main/java/com/example/tutorial/service/impl/OutputServiceImpl.java
@@ -1,14 +1,35 @@
 package com.example.tutorial.service.impl;
 
 import com.example.tutorial.service.OutputService;
+import com.example.tutorial.utility.CSVUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.ons.ctp.response.action.message.instruction.ActionContact;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionRequest;
+
+import java.io.FileWriter;
+import java.util.Arrays;
 
 @Slf4j
 @Service
 public class OutputServiceImpl implements OutputService {
+
   public void process(ActionRequest actionRequest) {
     log.debug("Entering process with actionRequest {}", actionRequest);
+
+    ActionContact contact = actionRequest.getContact();
+    if (contact != null) {
+      String csvFile = "/Users/philippebrossier/code_perso/rabbitMQwork/queueDump.csv";
+      try {
+        FileWriter writer = new FileWriter(csvFile, true);
+        CSVUtils.writeLine(writer, Arrays.asList(contact.getForename(),
+            contact.getSurname(),
+            contact.getEmailAddress()));
+        writer.flush();
+        writer.close();
+      } catch (Exception e) {
+        log.error("Exception with msg {} and cause {}", e.getMessage(), e.getCause());
+      }
+    }
   }
 }
